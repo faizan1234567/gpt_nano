ba8cb0c9f6791001dbe2de0b0c84556fb1cc6a49
MFaizan <mohammadfaizantanoli@gmail.com>
2024-06-30 19:59:30 +0500

    added mlp layer

--- model.py
+++ model.py
@@ -42,12 +42,50 @@ class Head(nn.Module):
         out = attn_wei @ v
         return out
 
+
+class MultiHeadAttention(nn.Module):
+    """multi head in parrellel"""
+    def __init__(self, num_heads: int = 4, n_emb:int =32, head_dim: int = 16, dropout: float = 0.3):
+        super().__init__()
+        self.num_heads = num_heads
+        self.head_size = head_dim
+        self.dropout = dropout
+
+        self.heads = [Head(n_emb=n_emb, head_dim= head_dim) for _ in range(num_heads)]
+        self.proj = nn.Linear(head_dim * num_heads, n_emb)
+        self.dropout = nn.Dropout(self.dropout)
+
+    def forward(self, x):
+        x = torch.cat([head(x) for head in self.heads], dim=-1)
+        x = self.proj(x)
+        x = self.dropout(x)
+        return x
+
+class MLP(nn.Module):
+    """feed forward layer followed by non linearity"""
+    def __init__(self, n_emb: int, dropout: float = 0.2):
+        super().__init__()
+        self.n_emb = n_emb
+        self.dropout = dropout
+        self.net = nn.Sequential((n_emb, 4*n_emb), 
+                                 nn.ReLU(), 
+                                 (4*n_emb, n_emb), 
+                                 nn.Dropout(dropout))
+    
+    def forward(self, x):
+        x = self.net(x)
+        return x
+
+
+
+
+
 # test the code
 if __name__ == "__main__":
     emb_dim = 32
     head_dim = 16
     dropout = 0.3
-    attention_head = Head(emb_dim, head_dim=head_dim, dropout=dropout)
+    attention_head = MultiHeadAttention(num_heads=4, n_emb=emb_dim, head_dim=head_dim, dropout=dropout)
     x = torch.rand(4, 8, 32)
     output = attention_head(x)
     print(output.shape) 
