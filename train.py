4bb7f3e1e8434dadbbb9cba79856ffa5e2e8898a
MFaizan <mohammadfaizantanoli@gmail.com>
2024-07-02 20:44:28 +0500

    testing without training

--- train.py
+++ train.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
         model = BigramLanguageModel(vocab_size)
     
     # Push model to device
-    model = model.to(device)
+    model = model
     
     # Prepare dataset
     dataset = getDataset(text_file=config.dataset.fname, block_size=config.general.block_size, 
